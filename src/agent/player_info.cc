f5c80cdb04a2971dede81da1c592f05469f0326e
mzzy11 <143632490+mzzy11@users.noreply.github.com>
2024-05-09 01:28:42 +0800

    fix style in player_info.cc

--- src/agent/player_info.cc
+++ src/agent/player_info.cc
@@ -30,10 +30,10 @@ auto format_as(Item const& object) -> std::string {
 auto format_as(PlayerInfo const& object) -> std::string {
   return fmt::format(
       "PlayerInfo{{id: {}, armor: {}, current_armor_health: {}, health: {}, "
-      "speed: {}, firearm: {}, firearms_pool:{}, range: {}, " 
+      "speed: {}, firearm: {}, firearms_pool:{}, range: {}, "
       "position: {}, inventory: {}}}",
-      object.id, object.armor, object.current_armor_health, object.health, 
-      object.speed, object.firearm, object.firearms_pool, object.range, 
+      object.id, object.armor, object.current_armor_health, object.health,
+      object.speed, object.firearm, object.firearms_pool, object.range,
       object.position, object.inventory);
 }
 
